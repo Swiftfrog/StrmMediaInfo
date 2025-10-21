5d1be8b17a50d72b905572fdd2264dfbfccd4004
albert <swiftfrog@gmail.com>
2025-10-21 12:25:46 +0800

    test.1

--- Evermedia/Configuration/PluginConfiguration.cs
+++ Evermedia/Configuration/PluginConfiguration.cs
@@ -1,6 +1,6 @@
 using MediaBrowser.Model.Plugins;
 
-namespace StrmMediaInfo.Configuration
+namespace Evermedia.Configuration
 {
     public class PluginConfiguration : BasePluginConfiguration
     {
